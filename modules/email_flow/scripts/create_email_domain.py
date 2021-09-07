6edbe10f7cbfdcbb552fabbc89379cd9ade1102f
john.carnell@genesys.com <john.carnell@genesys.com>
2021-09-07 14:16:07 -0400

    Trying to add the email_domain_route

---
+++ modules/email_flow/scripts/create_email_domain.py
@@ -0,0 +1,43 @@
+import subprocess
+import sys
+import os
+import time
+import PureCloudPlatformClientV2
+
+CLIENT_ID = os.environ["GENESYSCLOUD_OAUTHCLIENT_ID"]
+CLIENT_SECRET = os.environ["GENESYSCLOUD_OAUTHCLIENT_SECRET"]
+CLIENT_REGION = os.environ["GENESYSCLOUD_REGION"]
+TARGET_DOMAIN = os.environ["GENESYSCLOUD_EMAIL_DOMAIN"]
+TARGET_DOMAIN_NAME = os.environ["GENESYSCLOUD_EMAIL_DOMAIN_REGION"]
+FLOW_ID = os.environ["GENESYSCLOUD_FLOW_ID"]
+
+PureCloudPlatformClientV2.configuration.host = 	CLIENT_API_REGION
+apiClient = PureCloudPlatformClientV2.api_client.ApiClient().get_client_credentials_token(CLIENT_ID, CLIENT_SECRET)
+architectApi = PureCloudPlatformClientV2.ArchitectApi(apiClient)
+routingApi = PureCloudPlatformClientV2.RoutingApi(apiClient)
+
+def deleteEmailRoute():
+    print("\nDeleting email route for target domain: \n")
+    results = routingApi.get_routing_email_domain_routes(TARGET_DOMAIN)
+    
+    if len(results.entities)>0:
+        routeId = results.entities[0].id
+        routingApi.delete_routing_email_domain(routeId)
+        print("Successfully deleted email route for target domain: {}".format(TARGET_DOMAIN))
+
+def createEmailRoute():
+    print("Creating email route 'support' for flow id: {}\n".format(FLOW_ID))
+
+    body = PureCloudPlatformClientV2.InboundRoute() 
+    flow = PureCloudPlatformClientV2.DomainEntityRef()
+    flow.id=FLOW_ID
+    body.pattern="support"
+    body.from_name="Financial Services Support"
+    body.from_email= "support@" + TARGET_DOMAIN + "." + TARGET_DOMAIN_NAME
+    body.flow=flow
+    
+    routingApi.post_routing_email_domain_routes(TARGET_DOMAIN + "." + TARGET_DOMAIN_NAME,body)
+    print("Email route 'support' created for flow id: {}\n".format(FLOW_ID))
+
+deleteEmailRoute()
+createEmailRoute()
