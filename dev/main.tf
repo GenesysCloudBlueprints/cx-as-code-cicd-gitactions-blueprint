a661174d62e9623500d2894ef5c390f0b919cd6a
john.carnell@genesys.com <john.carnell@genesys.com>
2021-09-10 12:49:59 -0400

    Added a queue

--- dev/main.tf
+++ dev/main.tf
@@ -25,7 +25,7 @@ module "classifier_users" {
 
 module "classifier_queues" {
   source                   = "../modules/queues"
-  classifier_queue_names   = ["401K", "IRA", "529", "GeneralSupport", "ROTH"]
+  classifier_queue_names   = ["401K", "IRA", "529", "GeneralSupport", "ROTH", "ROTH2"]
   classifier_queue_members = module.classifier_users.user_ids
 }
 
