ce6da727fd742b18726279f5938ec265ba8180b7
john.carnell@genesys.com <john.carnell@genesys.com>
2021-09-14 08:57:02 -0400

    Adding real platform tests

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This is a WIP blueprint that will demonstrate how to build a CI/CD pipeline using CX-as-Code, Archy, GitHub Actions and Terraform Cloud.
 
-This blueprint has not been formally content or reviewed `yet, so use at your own caution.
+This blueprint has not been formally content or reviewed yet, so use at your own caution.
---
+++ scripts/platform_tests.py
@@ -0,0 +1,36 @@
+import subprocess
+import sys
+import os
+import time
+import PureCloudPlatformClientV2
+
+CLIENT_ID = os.environ["GENESYSCLOUD_OAUTHCLIENT_ID"]
+CLIENT_SECRET = os.environ["GENESYSCLOUD_OAUTHCLIENT_SECRET"]
+CLIENT_REGION = os.environ["GENESYSCLOUD_REGION"]
+CLIENT_API_REGION = os.environ["GENESYSCLOUD_API_REGION"]
+
+PureCloudPlatformClientV2.configuration.host = 	CLIENT_API_REGION
+apiClient = PureCloudPlatformClientV2.api_client.ApiClient().get_client_credentials_token(CLIENT_ID, CLIENT_SECRET)
+routingApi = PureCloudPlatformClientV2.RoutingApi(apiClient)
+
+def findQueue(queueName):
+  results = routingApi.get_routing_queues(name=queueName)
+
+  if len(results.entities)==1:
+    #print(results.entities)
+    return results.entities[0]
+  else: 
+    return nil
+
+def checkQueues():
+  ira = findQueue("IRA")
+  K401 = findQueue("401K") 
+  CS529 = findQueue("529") 
+  GS    = findQueue("GeneralSupport")   
+  
+  assert (ira.name=="IRA")==True,   "Retrieved IRA queue name does not match"
+  assert (K401.name=="401K")==True, "Retrieved 401K queue name does not match"
+  assert (CS529.name=="529")==True, "Retrieved 529 queue name does not match"
+  assert (GS.name=="GeneralSupport")==True, "Retrieved IRA queue name does not match"  
+
+checkQueues()
