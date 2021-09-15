4520c567096869421b2d279df2cfd4d6b5009862
john.carnell@genesys.com <john.carnell@genesys.com>
2021-09-15 10:42:31 -0400

    Added a check for the integration action

--- scripts/platform_tests.py
+++ scripts/platform_tests.py
@@ -12,6 +12,7 @@ CLIENT_API_REGION = os.environ["GENESYSCLOUD_API_REGION"]
 PureCloudPlatformClientV2.configuration.host = 	CLIENT_API_REGION
 apiClient = PureCloudPlatformClientV2.api_client.ApiClient().get_client_credentials_token(CLIENT_ID, CLIENT_SECRET)
 routingApi = PureCloudPlatformClientV2.RoutingApi(apiClient)
+integrationsApi = PureCloudPlatformClientV2.IntegrationsApi(apiClient)
 
 def findQueue(queueName):
   results = routingApi.get_routing_queues(name=queueName)
@@ -22,6 +23,13 @@ def findQueue(queueName):
   else: 
     return None
 
+def findIntegrationAction(actionName):
+  results = integrationsApi.get_integrations_action(name=actionName)
+
+  if len(results.entities)==1:
+    return results.entities[0]
+  else: 
+    return None    
 
 def checkQueues():
   ira = findQueue("IRA")
@@ -29,15 +37,21 @@ def checkQueues():
   CS529 = findQueue("529") 
   GS    = findQueue("GeneralSupport")   
   
-  assert (ira is None)==False
-  assert (K401 is None)==False
-  assert (CS529 is None)==False
-  assert (GS is None)==False
+  assert not(ira is None)
+  assert not(K401 is None)
+  assert not(CS529 is None)
+  assert not(GS is None)
   
   assert (ira.name=="IRA")==True,   "Retrieved IRA queue name does not match"
   assert (K401.name=="401K")==True, "Retrieved 401K queue name does not match"
   assert (CS529.name=="529")==True, "Retrieved 529 queue name does not match"
   assert (GS.name=="GeneralSupport")==True, "Retrieved IRA queue name does not match"  
 
+def checkIntegrationAction():
+  comprehendDataAction = findIntegrationAction("ComprehendDataAction")  
+
+  assert not(comprehendDataAction is None)
+
 #adding check
 checkQueues()
+checkIntegrationAction()
