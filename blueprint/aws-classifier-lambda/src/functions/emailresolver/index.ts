c0efd4536f4a073a3f479ff451ac786f8b34b0cc
john.carnell@genesys.com <john.carnell@genesys.com>
2021-09-27 16:40:04 -0400

    Interim commit for blueprint.

---
+++ blueprint/aws-classifier-lambda/src/functions/emailresolver/index.ts
@@ -0,0 +1,20 @@
+import schema from './schema';
+import { handlerPath } from '@libs/handlerResolver';
+
+export default {
+  handler: `${handlerPath(__dirname)}/handler.main`,
+  events: [
+    {
+      http: {
+        method: 'post',
+        path: 'emailresolver',
+        private: true,
+        request: {
+          schema: {
+            'application/json': schema
+          }
+        }
+      }
+    }
+  ]
+}
