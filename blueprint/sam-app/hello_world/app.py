501939416da2c569ac53a0adda518022eccd7673
Marlowe Tutor <marlowe.tutor@genesys.com>
2024-08-07 20:15:53 +0800

    Committed changes to work with the comprehend endpoint

--- blueprint/sam-app/hello_world/app.py
+++ blueprint/sam-app/hello_world/app.py
@@ -1,42 +1,14 @@
 import json
-
-# import requests
-
+import boto3
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
+    comprehend_object = boto3.client("comprehend")
+    some_string = "some random string"
+    response_object = comprehend_object.classify_document(   
+        Text=some_string,
+        EndpointArn='arn:aws:comprehend:us-east-1:435681138675:document-classifier-endpoint/emailclassifier'
+    )
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
+        "body": json.dumps(response_object),
     }
