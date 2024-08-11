4921f2a6eab4a67e1baac0e3976b8a86261a2bac
Marlowe Tutor <marlowe.tutor@genesys.com>
2024-08-11 12:34:08 +0800

    Committed changes to code regarding event actions

--- blueprint/sam-app/hello_world/app.py
+++ blueprint/sam-app/hello_world/app.py
@@ -1,9 +1,15 @@
 import json
 import boto3
+import logging
+logger = logging.getLogger()
+logger.setLevel("INFO")
 
 def lambda_handler(event, context):
+
+    some_string = event.get("body")
+    logger.info(json.dumps(some_string))
     comprehend_object = boto3.client("comprehend")
-    some_string = "some random string"
+    
     response_object = comprehend_object.classify_document(   
         Text=some_string,
         EndpointArn='arn:aws:comprehend:us-east-1:435681138675:document-classifier-endpoint/emailclassifier'
@@ -12,3 +18,4 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": json.dumps(response_object),
     }
+
