1448a0e5ab133fccece6e1ce92bf956b4126cbf5
Marlowe Tutor <marlowe.tutor@genesys.com>
2024-08-13 20:06:07 +0800

    Committed changes for initial implementation of API Key

--- blueprint/aws-sam-email-classifier/emailclassifier/app.py
+++ blueprint/aws-sam-email-classifier/emailclassifier/app.py
@@ -7,14 +7,17 @@ logger = logging.getLogger()
 logger.setLevel("INFO")
 
 def lambda_handler(event, context):
+
     account_id = os.getenv('AWS_ACCOUNT_ID')
     region = os.getenv('AWS_REGION')
-    some_string = event.get("body")
-    logger.info(json.dumps(some_string))
+
+    request_body_string= event.get("body")
+    logger.info(json.dumps(request_body_string))
+
     comprehend_object = boto3.client("comprehend")
     endpoint_arn = f'arn:aws:comprehend:{region}:{account_id}:document-classifier-endpoint/emailclassifier'
     response_object = comprehend_object.classify_document(   
-        Text=some_string,
+        Text=request_body_string,
         EndpointArn=endpoint_arn
     )
     return {
