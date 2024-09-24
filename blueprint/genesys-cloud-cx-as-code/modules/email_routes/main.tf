6874c8615c6199094b150ce43cfc0d26f680bad1
John Carnell <john.carnell@genesys.com>
2024-09-24 15:42:25 -0400

    Latest push:

--- blueprint/genesys-cloud-cx-as-code/modules/email_routes/main.tf
+++ blueprint/genesys-cloud-cx-as-code/modules/email_routes/main.tf
@@ -24,6 +24,6 @@ resource "genesyscloud_routing_email_route" "devengage_email_domain" {
   domain_id    = var.genesys_email_domain
   pattern="support"
   from_name="Financial Services"
-  from_email   = "support@${var.genesys_email_domain}.${genesys_email_domain_region}"
+  from_email   = "support@${var.genesys_email_domain}.${var.genesys_email_domain_region}"
   flow_id      = var.genesys_email_flow_id
 }
