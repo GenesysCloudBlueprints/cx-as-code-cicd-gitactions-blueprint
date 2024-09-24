146201d24ca28790d4edbdb0b91fc45ab9454f45
John Carnell <john.carnell@genesys.com>
2024-09-24 14:05:35 -0400

    Latest push:

--- blueprint/genesys-cloud-cx-as-code/modules/deploy_flows/main.tf
+++ blueprint/genesys-cloud-cx-as-code/modules/deploy_flows/main.tf
@@ -9,6 +9,6 @@ terraform {
 }
 
 resource "genesyscloud_flow" "classifier_flow" {
-  filepath          = "./deploy_flows/EmailComprehendFlow.yaml"
-  file_content_hash = filesha256("./deploy_flows/EmailComprehendFlow.yaml")
+  filepath          = "./modules/deploy_flows/EmailComprehendFlow.yaml"
+  file_content_hash = filesha256("./modules/deploy_flows/EmailComprehendFlow.yaml")
 }
