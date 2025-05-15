552c25b8d2b7885f29f98a4b3209012098a09d99
Marlowe Tutor <marlowe.c.tutor@gmail.com>
2025-05-15 09:43:42 +0800

    DEVEVANG-226 - Merged changes from branch

--- blueprint/genesys-cloud-cx-as-code/modules/email_routes/variables.tf
+++ blueprint/genesys-cloud-cx-as-code/modules/email_routes/variables.tf
@@ -2,3 +2,13 @@ variable "genesys_email_domain" {
   type        = string
   description = "The name of the domain.  This is used to help build the email route"
 }
+
+variable "genesys_email_domain_region" {
+  type        = string
+  description = "The name of the email region.  This is used to help build the email route"
+}
+
+variable "genesys_email_flow_id" {
+  type        = string
+  description = "The flow that will be executed on the inbound email."
+}
